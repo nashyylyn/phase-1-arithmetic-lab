c4e77b024780b3730c6a7ff43228e6a2b7193b59
nashylyn <natasha.kamau@student.moringaschool,com>
2025-03-11 12:33:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+ let multiply = num2*num1;
+ let random = 1;
+ let num3 = 10
+ let num4 = 6
+ let mod = num3 % num4
+ let max =  Math.max(5,10,15,20);
+ 
